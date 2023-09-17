8b17e1d22f4edb4019dfdfe8a90062d9d8654241
Mark Taylor <m.b.taylor@bristol.ac.uk>
2023-09-17 17:54:34 +0100

    Fix fencepost bug

--- Game.java
+++ Game.java
@@ -11,7 +11,7 @@ public class Game {
     public static int keep( int r, int k, boolean isExplode ) {
         int[] rolls = new int[ r ];
         for ( int i = 0; i < r; i++ ) {
-            rolls[ i ] = 1 + rnd_.nextInt( 10 );
+            rolls[ i ] = d10();
         }
         Arrays.sort( rolls );
         int total = 0;
@@ -22,8 +22,13 @@ public class Game {
         return total;
     }
 
+    @StateDependent
+    public static int d10() {
+        return 1 + rnd_.nextInt( 10 );
+    }
+
     private static int maybeExplode( int c ) {
-        return c == 10 ? c + maybeExplode( rnd_.nextInt( 10 ) )
+        return c == 10 ? c + maybeExplode( d10() )
                        : c;
     }
 }
